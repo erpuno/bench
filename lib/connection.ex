a6bfbc1f677d3fc4145a3bfa09fa9a1c9eedeae2
Namdak Tonpa <maxim@synrc.com>
2019-07-20 18:39:56 +0300

    wip

--- lib/connection.ex
+++ lib/connection.ex
@@ -2,8 +2,9 @@ defmodule Connection do
   use N2O
 
   def proc(:init, p) do
-    {:ok, conn} = :gun.open('localhost', 8042, %{protocols: [:http], transport: :ssl})
-    {:ok, pi(p, state: {conn,[]})}
+    {:ok, conn} = :gun.open('localhost', 8042, %{protocols: [:http], transport: :tls})
+    ref = :gun.ws_upgrade(conn, "/ws", [])
+    {:ok, pi(p, state: {conn,ref})}
   end
 
   def proc({:gun_up, conn, :http}, p) do
@@ -12,23 +13,30 @@ defmodule Connection do
     {:reply, [], pi(p, state: {conn,ref})}
   end
 
-  def proc({:gun_upgrade, conn, ref, _, headers}, pi(state: {conn,ref}) = p) do
+  def proc({:gun_upgrade, conn, ref, proto, headers}, pi(state: {conn,ref}) = p) do
     IO.inspect "GUN UPGRADE"
-    case :gun.await(conn, ref) do
+    case :gun.await(conn,ref) do
     {:upgrade, _, _} -> IO.inspect("Successful WebSocket Upgrade")
-                     {:reply, [], p}
-               p -> IO.inspect("Upgrade Error: #{inspect(p)}")
-                     {:ok, p}   
+                        {:reply, [], p}
+                   x -> IO.inspect("Upgrade Error: #{inspect(x)}")
+                        {:reply, [], p}
     end
   end
 
-  def proc({:gun_down, _conn, :http, :closed, _, _}, p) do
+  def proc({:gun_down, _conn, _proto, _status, _, _}, p) do
     IO.inspect("GUN DOWN")
+    {:ok, conn} = :gun.open('localhost', 8042, %{protocols: [:http], transport: :tls})
+    {:reply, [], pi(p, state: {conn,[]})}
+  end
+
+  def proc({:gun_ws, pid, ref, msg}, p) do
+    IO.inspect "GUN INCOME"
+    IO.inspect msg
     {:reply, [], p}
   end
 
-  def proc({:send_msg, message}, pi(state: conn) = p) do
-    IO.inspect :gun.ws_send(conn, message)
+  def proc({:send_msg, message}, pi(state: {conn,_}) = p) do
+    IO.inspect :gun.ws_send(conn, {:text, message})
     {:reply, [], p}
   end
 
